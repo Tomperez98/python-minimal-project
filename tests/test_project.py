77928d79c5afa698d1fed0833184d3637749398f
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-08-17 10:28:33 -0500

    Coverage back to 100

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,7 +1,7 @@
-# from template_project import VERSION, VERSION_SHORT
+from template_project import VERSION, VERSION_SHORT
 
 
 def test_version():
-    assert True
-    # assert VERSION == "0.1.0."
-    # assert VERSION_SHORT == "0.1"
+
+    assert VERSION == "0.1.0."
+    assert VERSION_SHORT == "0.1"
