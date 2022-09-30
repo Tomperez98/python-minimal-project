c5ff0513a509b701950547e7088bf3d1b7c6d8f7
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-09-30 12:38:56 -0500

    Rich documentation for local development

--- template_project/_logger.py
+++ template_project/_logger.py
@@ -1,24 +1,43 @@
 import logging
+import sys
 
 import orjson
 import structlog
 
+shared_processors = [
+    # Processors that have nothing to do with output,
+    # e.g., add timestamps or log level names.
+    structlog.contextvars.merge_contextvars,
+    structlog.processors.add_log_level,
+    structlog.processors.TimeStamper(fmt="iso", utc=True),
+    structlog.processors.CallsiteParameterAdder(
+        {
+            structlog.processors.CallsiteParameter.FILENAME,
+            structlog.processors.CallsiteParameter.FUNC_NAME,
+            structlog.processors.CallsiteParameter.LINENO,
+        }
+    ),
+    structlog.processors.format_exc_info,
+]
+if sys.stderr.isatty():
+    processors = shared_processors + [
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+        structlog.dev.ConsoleRenderer(),
+    ]
+    factory = structlog.PrintLoggerFactory()
+
+else:
+    processors = shared_processors + [
+        structlog.processors.dict_tracebacks,
+        structlog.processors.JSONRenderer(serializer=orjson.dumps),
+    ]
+    factory = structlog.BytesLoggerFactory()
+
+
 structlog.configure(
     cache_logger_on_first_use=True,
     wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
-    processors=[
-        structlog.contextvars.merge_contextvars,
-        structlog.processors.add_log_level,
-        structlog.processors.CallsiteParameterAdder(
-            {
-                structlog.processors.CallsiteParameter.FILENAME,
-                structlog.processors.CallsiteParameter.FUNC_NAME,
-                structlog.processors.CallsiteParameter.LINENO,
-            }
-        ),
-        structlog.processors.format_exc_info,
-        structlog.processors.TimeStamper(fmt="iso", utc=True),
-        structlog.processors.JSONRenderer(serializer=orjson.dumps),
-    ],
-    logger_factory=structlog.BytesLoggerFactory(),
+    processors=processors,
+    logger_factory=factory,
 )
