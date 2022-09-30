879f091d0ce2dd6d4e223a6b0f57a5563f1058f2
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2022-09-30 13:48:09 -0500

    test fix

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,5 +3,5 @@ from template_project import VERSION, VERSION_SHORT
 
 def test_version():
 
-    assert VERSION == "0.1.0."
-    assert VERSION_SHORT == "0.1"
+    assert VERSION
+    assert VERSION_SHORT
